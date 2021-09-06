f8eba32cb8673401033ef244a2ad94c0f93ca654
Arno Moonens <arno.moonens@gmail.com>
2021-09-07 00:15:20 +0200

    Main function to train and get score

--- train_predict.py
+++ train_predict.py
@@ -1,15 +1,19 @@
 """Train and predict on the given data."""
 
+import argparse
+from pathlib import Path
 from typing import Tuple
 import pandas as pd
 import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MultiLabelBinarizer, OneHotEncoder
 from preprocessing import clean_data
+from scoring import mapk
 
 def train(df: pd.DataFrame) -> Tuple[MultiLabelBinarizer, Pipeline]:
     """Train a predictive model to rank movie genres based on their synopsis."""
@@ -42,7 +46,7 @@ def train(df: pd.DataFrame) -> Tuple[MultiLabelBinarizer, Pipeline]:
     return multilabel_binarizer, pipe
 
 
-def predict(multilabel_binarizer: MultiLabelBinarizer, pipe: Pipeline, df: pd.DataFrame) -> PlainTextResponse:
+def predict(multilabel_binarizer: MultiLabelBinarizer, pipe: Pipeline, df: pd.DataFrame) -> pd.DataFrame:
     """Predict the movie genres for each synopsis in the given file."""
     # Load and clean data like in train function
     df["synopsis"] = clean_data(df["synopsis"])
@@ -61,3 +65,35 @@ def predict(multilabel_binarizer: MultiLabelBinarizer, pipe: Pipeline, df: pd.Da
                               "genres": output_genres})
 
     return result_df
+
+
+def get_score(predictions_df: pd.DataFrame,
+              real_df: pd.DataFrame,
+              k: int = 5) -> float:
+    """Get the MAPK score for the given predictions."""
+    y_pred = predictions_df.genres.apply(lambda x: x.split(" "))
+    y_real = real_df.genres.apply(lambda x: x.split(" "))
+    return mapk(y_real, y_pred, k)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_file", type=Path, help="Path to training data")
+    # parser.add_argument("test_file", type=Path, help="Path to test data")
+    parser.add_argument("--k", type=int, default=5, help="k value for MAPK scoring function")
+    args = parser.parse_args()
+
+    train_df = pd.read_csv(args.train_file)
+    train_df, val_df = train_test_split(train_df, test_size=.2)
+    mlb, pipe = train(train_df)
+
+    val_predictions = predict(mlb, pipe, val_df)
+    # test_df = pd.read_csv(args.test_file)
+    # test_predictions = predict(mlb, pipe, test_df)
+
+    score = get_score(val_predictions, val_df, args.k)
+    print(f"MAPK score on validation data: {score}")
+
+
+if __name__ == "__main__":
+    main()
